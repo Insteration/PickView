aadd4c5445045693058032218a28dd4e33950a81
Artem Karmaz <inste@icloud.com>
2018-12-21 20:15:54 +0200

    Initial Commit

---
+++ PickView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PickView
+//
+//  Created by Artem Karmaz on 12/21/18.
+//  Copyright © 2018 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
