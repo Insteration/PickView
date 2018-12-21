ee62adfaa951f9189de876b44a1650ccd0ef066c
Artem Karmaz <inste@icloud.com>
2018-12-21 20:40:47 +0200

    first commit

--- PickView/ViewController.swift
+++ PickView/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let cityArray = ["Kyiv", "Dnipro", "Lviv", "Kharkov", "Odessa"]
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var cityPickerView: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // delegate выолнение одного класса в другом
+        self.cityPickerView.delegate = self // изменение внесение данных
+        self.cityPickerView.dataSource = self // управление данных которые есть
+        
     }
-
-
+    
+    
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // количество столбцов
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.cityArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.cityArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.textLabel.text = self.cityArray[row]
+    }
+    
+}
